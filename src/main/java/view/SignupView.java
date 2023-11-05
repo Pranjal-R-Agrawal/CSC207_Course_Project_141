deee0ef2ff78a4df462b6afa2679c5182ce69ca0
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-11-05 17:36:44 -0500

    Added import

--- src/main/java/view/SignupView.java
+++ src/main/java/view/SignupView.java
@@ -4,6 +4,7 @@ import use_case.signup.interface_adapter.SignupController;
 import use_case.signup.interface_adapter.SignupState;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
