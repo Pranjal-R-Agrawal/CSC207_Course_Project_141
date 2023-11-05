4f96fc1c6ff37b3917220c0efe3d625cec8d84b9
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-11-05 01:23:01 -0400

    Added name, email and phoneNumber instance attributes to SignupInputData

--- src/main/java/use_case/signup/application_business_rules/SignupInputData.java
+++ src/main/java/use_case/signup/application_business_rules/SignupInputData.java
@@ -4,22 +4,41 @@ public class SignupInputData {
     final private String username;
     final private String password;
     final private String repeatPassword;
+    final private String name;
+    final private String email;
+    final private String phoneNumber;
 
-    public SignupInputData(String username, String password, String repeatPassword) {
+
+    public SignupInputData(String username, String password, String repeatPassword, String name, String email, String phoneNumber) {
         this.username = username;
         this.password = password;
         this.repeatPassword = repeatPassword;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
     }
 
-    String getUsername() {
+    public String getUsername() {
         return username;
     }
 
-    String getPassword() {
+    public String getPassword() {
         return password;
     }
 
     public String getRepeatPassword() {
         return repeatPassword;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 }
