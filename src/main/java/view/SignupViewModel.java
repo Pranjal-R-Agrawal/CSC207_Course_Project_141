b02d4d1fca8470a75acc90f4a468c8aaf360c2cf
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-11-05 16:20:21 -0500

    Created SignupViewModel to manage SignupView

---
+++ src/main/java/view/SignupViewModel.java
@@ -0,0 +1,40 @@
+package view;
+
+import use_case.signup.interface_adapter.SignupState;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class SignupViewModel extends ViewModel {
+    public static final String USERNAME_LABEL = "Enter Username";
+    public static final String PASSWORD_LABEL = "Enter Password";
+    public static final String REPEAT_PASSWORD_LABEL = "Repeat Password";
+    public static final String NAME_LABEL = "Enter Name";
+    public static final String PHONE_NUMBER_LABEL = "Enter Phone Number";
+    public static final String EMAIL_LABEL = "Enter Email ID";
+    public static final String OPTIONAL_LABEL = "Entering Phone Number is optional";
+    public static final String SIGNUP_BUTTON_LABEL = "Entering Phone Number is optional";
+    private SignupState state = new SignupState();
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public SignupState getState() {
+        return state;
+    }
+
+    public void setState(SignupState state) {
+        this.state = state;
+    }
+
+    public SignupViewModel() {
+        super("signup");
+    }
+
+    public void firePropertyChanged(String propertyName) {
+        support.firePropertyChange(propertyName, null, this.state);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+}
