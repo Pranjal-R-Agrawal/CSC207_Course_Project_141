950a82e1cc8c2ca098a145ea9b79f8051ea80163
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-11-05 16:49:33 -0500

    Created panels for each input

--- src/main/java/view/SignupView.java
+++ src/main/java/view/SignupView.java
@@ -1,6 +1,7 @@
 package view;
 
 import use_case.signup.interface_adapter.SignupController;
+import use_case.signup.interface_adapter.SignupState;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -14,6 +15,9 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
     private final JTextField usernameInputField = new JTextField(20);
     private final JPasswordField passwordInputField = new JPasswordField(20);
     private final JPasswordField repeatPasswordInputField = new JPasswordField(20);
+    private final JPasswordField nameInputField = new JPasswordField(20);
+    private final JPasswordField phoneNumberInputField = new JPasswordField(20);
+    private final JPasswordField emailInputField = new JPasswordField(20);
     private final SignupController signupController;
     private final JButton signupButton = new JButton();
 
@@ -25,8 +29,23 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
         signupButton.setText(SignupViewModel.SIGNUP_BUTTON_LABEL);
 
         signupViewModel.addPropertyChangeListener(this);
+
+        JPanel usernamePanel = createEntry(SignupViewModel.USERNAME_LABEL, usernameInputField);
+        JPanel passwordPanel = createEntry(SignupViewModel.PASSWORD_LABEL, passwordInputField);
+        JPanel repeatPasswordPanel = createEntry(SignupViewModel.REPEAT_PASSWORD_LABEL, repeatPasswordInputField);
+        JPanel namePanel = createEntry(SignupViewModel.NAME_LABEL, nameInputField);
+        JPanel phoneNumberPanel = createEntry(SignupViewModel.PHONE_NUMBER_LABEL, phoneNumberInputField);
+        JPanel emailPanel = createEntry(SignupViewModel.EMAIL_LABEL, emailInputField);
+        JPanel optionalPanel = new JPanel();
+        optionalPanel.add(new JLabel(SignupViewModel.OPTIONAL_LABEL));
     }
 
+    private JPanel createEntry(String name, JTextField textField) {
+        JPanel panel = new JPanel();
+        panel.add(new JLabel(name));
+        panel.add(textField);
+        return panel;
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
