15a48e9317e3b5ad3744e5acbf87c4e3ab1f5789
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-11-05 15:52:29 -0500

    Created SignupState for storing the state of SignupView

---
+++ src/main/java/use_case/signup/interface_adapter/SignupState.java
@@ -0,0 +1,80 @@
+package use_case.signup.interface_adapter;
+
+public class SignupState {
+    private String username;
+    private String password;
+    private String repeatPassword;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String errorMessage;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    @Override
+    public String toString() {
+        return "SignupState{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", repeatPassword='" + repeatPassword + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", errorMessage='" + errorMessage + '\'' +
+                '}';
+    }
+}
