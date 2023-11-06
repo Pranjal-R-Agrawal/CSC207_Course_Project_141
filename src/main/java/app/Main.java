14bfa6e7fdeab2213ac32094b0675b5d07ffaab7
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-11-06 11:50:56 -0500

    Main now creates SignupView and all the other components required to link together the UI

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,38 @@
 package app;
 
+import data_access.MongoDBDataAccessObject;
+import view.SignupView;
+import view.SignupViewModel;
+import view.ViewManager;
+import view.ViewManagerModel;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame application = new JFrame("Startup Generator");
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        CardLayout cardLayout = new CardLayout();
+
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        SignupViewModel signupViewModel = new SignupViewModel();
+
+        MongoDBDataAccessObject mongoDBDataAccessObject = new MongoDBDataAccessObject();
+
+        SignupView signupView = SignupUseCaseFactory.create(viewManagerModel, signupViewModel, mongoDBDataAccessObject);
+        views.add(signupView, signupView.viewName);
+
+        viewManagerModel.setActiveView(signupView.viewName);
+        viewManagerModel.firePropertyChanged();
+
+        application.pack();
+        application.setVisible(true);
     }
 }
