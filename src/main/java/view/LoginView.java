890b17b687d58e4883e4af887c322c232947a087
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-11-07 12:39:50 -0500

    Added helper method createEntry to create panels containing a label and an input field and the method addComponent to add the panel to the GridBagLayout

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -3,6 +3,7 @@ package view;
 import use_case.login.interface_adapter.LoginController;
 
 import javax.swing.*;
+import java.awt.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
@@ -20,8 +21,23 @@ public class LoginView extends JPanel implements PropertyChangeListener {
 
         viewName = loginViewModel.getViewName();
         setName(viewName);
+        loginButton.setText(LoginViewModel.LOGIN_BUTTON_LABEL);
 
         loginViewModel.addPropertyChangeListener(this);
+    }
+
+    private void addComponent(JPanel panel, GridBagConstraints c, int gridx, int gridy, int fill) {
+        c.gridx = gridx;
+        c.gridy = gridy;
+        c.fill = fill;
+        add(panel, c);
+    }
+
+    private JPanel createEntry(String name, JTextField textField) {
+        JPanel panel = new JPanel();
+        panel.add(new JLabel(name));
+        panel.add(textField);
+        return panel;
     }
 
     @Override
