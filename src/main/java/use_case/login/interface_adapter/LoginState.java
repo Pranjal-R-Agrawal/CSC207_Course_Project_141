c9e6de202ba36e614d0aa4215a0cade6d29c0265
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-11-07 12:14:49 -0500

    Created LoginState for storing the state of LoginView

---
+++ src/main/java/use_case/login/interface_adapter/LoginState.java
@@ -0,0 +1,31 @@
+package use_case.login.interface_adapter;
+
+public class LoginState {
+    private String username;
+    private String password;
+    private String errorMessage;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
