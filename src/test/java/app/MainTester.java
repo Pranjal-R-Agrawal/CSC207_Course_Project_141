6ab7c2eabf29d51ca5f433a876d4954fcf72ed36
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-11-07 23:00:37 -0500

    Implemented getCurrentView method in MainTester

--- src/test/java/app/MainTester.java
+++ src/test/java/app/MainTester.java
@@ -2,6 +2,9 @@ package app;
 
 import view.SignupViewModel;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class MainTester extends Main {
     public static void main(String[] args) {
         Main.main(new String[] {
@@ -12,5 +15,29 @@ public class MainTester extends Main {
 
     public static SignupViewModel getSignupViewModel() {
         return signupViewModel;
+    }
+
+    public static JPanel getCurrentView() {
+        JFrame application = null;
+        Window[] windows = Window.getWindows();
+
+        for (Window window : windows) {
+            if (window instanceof JFrame) {
+                application = (JFrame) window;
+            }
+        }
+
+        assert application != null;
+
+        JPanel root = (JPanel) ((JRootPane) application.getComponent(0)).getContentPane();
+        JPanel views = (JPanel) root.getComponent(0);
+
+        Component panel = views.getComponent(0);
+
+        for (Component component : views.getComponents()) {
+            if ((panel = component).isVisible()) break;
+        }
+
+        return (panel instanceof JPanel)? (JPanel) panel : null;
     }
 }
