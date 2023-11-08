b7d63cc88322cc2850b46422504672ed21b623fb
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-11-07 22:12:35 -0500

    Organised and simplified pre-test code

--- src/test/java/view/SignupViewTest.java
+++ src/test/java/view/SignupViewTest.java
@@ -1,134 +1,80 @@
 package view;
 
-import app.Main;
-
-import app.MainTester;
+import app.SignupUseCaseFactory;
 import data_access.MongoDBDataAccessObjectTest;
 import entity.User;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import use_case.signup.interface_adapter.SignupState;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
-import static org.junit.Assert.assertNotNull;
-
 public class SignupViewTest {
-    private JTextField usernameInputField;
-    private JPasswordField passwordInputField;
-    private JPasswordField repeatPasswordInputField;
-    private JTextField nameInputField;
-    private JTextField phoneNumberInputField;
-    private JTextField emailInputField;
-    private JButton signupButton;
     private static boolean popUpDiscovered = false;
     private static String message = "";
-    MongoDBDataAccessObjectTest database = new MongoDBDataAccessObjectTest();
+    private MongoDBDataAccessObjectTest dataAccessObject;
+    private SignupViewModel signupViewModel;
+    private JButton signupButton;
 
     @Test
     public void testAllFieldsEmpty() {
-        Main.main(new String[] {
-                "src/main/java/data_access/database_connection.txt",
-                "Tests", "Users", "Posts", "Comments"
-        });
-        getComponents();
-
         createCloseTimer().start();
         signupButton.doClick();
-
         assert message.contains("Please enter a username.");
     }
 
     @Test
     public void testUsernameEmpty() {
-        Main.main(new String[] {
-                "src/main/java/data_access/database_connection.txt",
-                "Tests", "Users", "Posts", "Comments"
-        });
-        getComponents();
-
         createCloseTimer().start();
         signupButton.doClick();
-
         assert message.contains("Please enter a username.");
     }
 
     @Test
     public void testPasswordEmpty() {
-        MainTester.main(null);
-        getComponents();
-
-        MainTester.getSignupViewModel().setState(
-                new SignupState().setUsername("username")
-        );
-
+        signupViewModel.getState().setUsername("username");
+        signupViewModel.firePropertyChanged("update_fields");
         createCloseTimer().start();
         signupButton.doClick();
-
         assert message.contains("Please enter a password.");
     }
 
     @Test
     public void testRepeatPasswordEmpty() {
-        MainTester.main(null);
-        getComponents();
-
-        MainTester.getSignupViewModel().setState(
-                new SignupState().setUsername("username").setPassword("password")
-        );
-
+        signupViewModel.getState().setUsername("username").setPassword("password");
+        signupViewModel.firePropertyChanged("update_fields");
         createCloseTimer().start();
         signupButton.doClick();
-
         assert message.contains("Please enter a password.");
     }
 
     @Test
     public void testNameEmpty() {
-        MainTester.main(null);
-        getComponents();
-
-        MainTester.getSignupViewModel().setState(
-                new SignupState().setUsername("username").setPassword("password").setRepeatPassword("password")
-        );
-
+        signupViewModel.getState().setUsername("username").setPassword("password")
+                .setRepeatPassword("password");
+        signupViewModel.firePropertyChanged("update_fields");
         createCloseTimer().start();
         signupButton.doClick();
-
         assert message.contains("Please enter your name.");
     }
 
     @Test
     public void testEmailEmpty() {
-        MainTester.main(null);
-        getComponents();
-
-        MainTester.getSignupViewModel().setState(
-                new SignupState().setUsername("username").setPassword("password")
-                        .setRepeatPassword("password").setName("name")
-        );
-
+        signupViewModel.getState().setUsername("username").setPassword("password")
+                .setRepeatPassword("password").setName("name");
+        signupViewModel.firePropertyChanged("update_fields");
         createCloseTimer().start();
         signupButton.doClick();
-
         assert message.contains("Please enter your email-id.");
     }
 
     @Test
     public void testPhoneNumberEmpty() {
-        MainTester.main(null);
-        getComponents();
-
-        MainTester.getSignupViewModel().setState(
-                new SignupState().setUsername("username").setPassword("password")
-                        .setRepeatPassword("password").setName("name").setEmail("email")
-        );
-
-        popUpDiscovered = false;
+        signupViewModel.getState().setUsername("username").setPassword("password")
+                .setRepeatPassword("password").setName("name").setEmail("email");
+        signupViewModel.firePropertyChanged("update_fields");
         createCloseTimer().start();
         signupButton.doClick();
         assert !popUpDiscovered;
@@ -136,15 +82,9 @@ public class SignupViewTest {
 
     @Test
     public void testUsernameUsedNoUsers() {
-        MainTester.main(null);
-        getComponents();
-
-        MainTester.getSignupViewModel().setState(
-                new SignupState().setUsername("username").setPassword("password")
-                        .setRepeatPassword("password").setName("name").setEmail("email")
-        );
-
-        popUpDiscovered = false;
+        signupViewModel.getState().setUsername("username").setPassword("password")
+                .setRepeatPassword("password").setName("name").setEmail("email");
+        signupViewModel.firePropertyChanged("update_fields");
         createCloseTimer().start();
         signupButton.doClick();
         assert !popUpDiscovered;
@@ -152,62 +92,25 @@ public class SignupViewTest {
 
     @Test
     public void testUsernameUsedAddUser() {
-        database.addUser(
+        dataAccessObject.addUser(
                 new User("username", "password", "name", "email", "phone")
         );
-
-        MainTester.main(null);
-        getComponents();
-
-        MainTester.getSignupViewModel().setState(
-                new SignupState().setUsername("username").setPassword("password")
-                        .setRepeatPassword("password").setName("name").setEmail("email")
-        );
-
+        signupViewModel.getState().setUsername("username").setPassword("password")
+                .setRepeatPassword("password").setName("name").setEmail("email");
+        signupViewModel.firePropertyChanged("update_fields");
         createCloseTimer().start();
         signupButton.doClick();
-
         assert message.contains("Username already used.");
     }
 
     @Test
     public void testPasswordsDontMatch() {
-        MainTester.main(null);
-        getComponents();
-
-        MainTester.getSignupViewModel().setState(
-                new SignupState().setUsername("username").setPassword("password")
-                        .setRepeatPassword("password2").setName("name").setEmail("email")
-        );
-
+        signupViewModel.getState().setUsername("username").setPassword("password1")
+                .setRepeatPassword("password2").setName("name").setEmail("email");
+        signupViewModel.firePropertyChanged("update_fields");
         createCloseTimer().start();
         signupButton.doClick();
-
         assert message.contains("Passwords don't match.");
-    }
-
-    public void getComponents() {
-        JFrame application = null;
-        Window[] windows = Window.getWindows();
-        for (Window window : windows) {
-            if (window instanceof JFrame) {
-                application = (JFrame) window;
-            }
-        }
-
-        assertNotNull(application);
-
-        JPanel root = (JPanel) ((JRootPane) application.getComponent(0)).getContentPane();
-        JPanel views = (JPanel) root.getComponent(0);
-        SignupView signupView = (SignupView) views.getComponent(0);
-
-        usernameInputField = (JTextField) ((JPanel) signupView.getComponent(0)).getComponent(1);
-        passwordInputField = (JPasswordField) ((JPanel) signupView.getComponent(1)).getComponent(1);
-        repeatPasswordInputField = (JPasswordField) ((JPanel) signupView.getComponent(2)).getComponent(1);
-        nameInputField = (JTextField) ((JPanel) signupView.getComponent(3)).getComponent(1);
-        phoneNumberInputField = (JTextField) ((JPanel) signupView.getComponent(4)).getComponent(1);
-        emailInputField = (JTextField) ((JPanel) signupView.getComponent(5)).getComponent(1);
-        signupButton = (JButton) signupView.getComponent(7);
     }
 
     private Timer createCloseTimer() {
@@ -237,8 +140,20 @@ public class SignupViewTest {
     }
 
     @Before
-    @After
-    public void resetDatabase() {
-        database.resetDatabase();
+    public void setUpTest() {
+        dataAccessObject = new MongoDBDataAccessObjectTest();
+        signupViewModel = new SignupViewModel();
+        SignupView signupView = SignupUseCaseFactory.create(new ViewManagerModel(), signupViewModel, new LoginViewModel(),dataAccessObject);
+        signupButton = (JButton) signupView.getComponent(7);
+
+        dataAccessObject.resetDatabase();
+
+        message = "";
+        popUpDiscovered = false;
+
+        JFrame application = new JFrame("Startup Generator");
+        application.add(signupView);
+        application.pack();
+        application.setVisible(true);
     }
 }
