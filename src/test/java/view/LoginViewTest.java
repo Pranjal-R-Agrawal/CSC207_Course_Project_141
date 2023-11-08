5695e4c5817faba952814783b601d761b2adf5c7
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-11-07 20:46:29 -0500

    Added unit test suite for LoginView

---
+++ src/test/java/view/LoginViewTest.java
@@ -0,0 +1,110 @@
+package view;
+
+import entity.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import app.LoginUseCaseFactory;
+import data_access.MongoDBDataAccessObjectTest;
+import use_case.login.interface_adapter.LoginState;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class LoginViewTest {
+    private static String message;
+    private static boolean popUpDiscovered;
+    private final MongoDBDataAccessObjectTest dataAccessObject = new MongoDBDataAccessObjectTest();
+    private final LoginViewModel loginViewModel = new LoginViewModel();
+    private final LoginView loginView = LoginUseCaseFactory.create(new ViewManagerModel(), loginViewModel, dataAccessObject);
+    private final JButton loginButton = (JButton) loginView.getComponent(2);
+
+    @Test
+    public void testAllFieldsEmpty () {
+        createCloseTimer().start();
+        loginButton.doClick();
+        assert popUpDiscovered && message.equals("Please enter a username.");
+    }
+
+    @Test
+    public void testUsernameEmpty () {
+        createCloseTimer().start();
+        loginButton.doClick();
+        assert popUpDiscovered && message.equals("Please enter a username.");
+    }
+
+    @Test
+    public void testPasswordEmpty () {
+        loginViewModel.getState().setUsername("username");
+        loginViewModel.firePropertyChanged("update_username");
+        createCloseTimer().start();
+        loginButton.doClick();
+        assert popUpDiscovered && message.equals("Please enter a password.");
+    }
+
+    @Test
+    public void testInvalidCredentials () {
+        loginViewModel.getState().setUsername("invalid");
+        loginViewModel.getState().setPassword("credentials");
+        loginViewModel.firePropertyChanged("update_username");
+        loginViewModel.firePropertyChanged("update_password");
+        createCloseTimer().start();
+        loginButton.doClick();
+        assert popUpDiscovered && message.equals("Invalid username or password.");
+    }
+
+    @Test
+    public void testValidCredentials () {
+        dataAccessObject.addUser(
+                new User("username", "password", "name", "email", "phone")
+        );
+        loginViewModel.getState().setUsername("username");
+        loginViewModel.getState().setPassword("password");
+        loginViewModel.firePropertyChanged("update_username");
+        loginViewModel.firePropertyChanged("update_password");
+        createCloseTimer().start();
+        loginButton.doClick();
+        assert !popUpDiscovered;
+    }
+
+    private Timer createCloseTimer() {
+        ActionListener close = e -> {
+            Window[] windows = Window.getWindows();
+            for (Window window : windows) {
+                if (window instanceof JDialog dialog) {
+                    if (dialog.isVisible()) {
+                        String s = ((JOptionPane) ((BorderLayout) dialog.getRootPane()
+                                .getContentPane().getLayout()).getLayoutComponent(BorderLayout.CENTER)).getMessage().toString();
+
+                        System.out.println("message = " + s);
+
+                        LoginViewTest.message = s;
+                        LoginViewTest.popUpDiscovered = true;
+                    }
+                }
+            }
+            for (Window window : windows) {
+                window.dispose();
+            }
+        };
+
+        Timer t = new Timer(1000, close);
+        t.setRepeats(false);
+        return t;
+    }
+
+    @Before
+    public void setupTest() {
+        dataAccessObject.resetDatabase();
+
+        message = null;
+        popUpDiscovered = false;
+
+        JFrame application = new JFrame("Startup Generator");
+        application.add(loginView);
+        application.pack();
+        application.setVisible(true);
+    }
+}
