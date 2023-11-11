2aaf386b69c7fae5f6f09df9bb752d75395d87fb
Yathusan <yathusank630@gmail.com>
2023-11-10 20:41:04 -0500

    Created CommentInputData to transfer information from the Controller to the Interactor

---
+++ src/main/java/use_case/comment/CommentInputData.java
@@ -0,0 +1,27 @@
+package use_case.comment;
+
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
+public class CommentInputData {
+    final private ObjectId parentId;
+    final private ObjectId authorId;
+    final private String body;
+    final private List<String> qualifications;
+    final private List<String> collaborationRoles;
+
+    public CommentInputData(ObjectId parentId, ObjectId authorId, String body, List<String> qualifications, List<String> collaborationRoles){
+        this.parentId = parentId;
+        this.authorId = authorId;
+        this.body = body;
+        this.qualifications = qualifications;
+        this.collaborationRoles = collaborationRoles;
+    }
+    public ObjectId getParentId(){return parentId;}
+    public ObjectId getAuthorId(){return authorId;}
+    public String getBody(){return body;}
+    public List<String> getQualifications(){return qualifications;}
+    public List<String> getCollaborationRoles(){return collaborationRoles;}
+
+}
