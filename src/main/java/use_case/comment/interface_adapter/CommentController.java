aaa05c58b7276212c7a3300d512eb2fd7e01071d
Yathusan <yathusank630@gmail.com>
2023-11-12 23:46:38 -0500

    Added CommentController

---
+++ src/main/java/use_case/comment/interface_adapter/CommentController.java
@@ -0,0 +1,20 @@
+package use_case.comment.interface_adapter;
+
+import org.bson.types.ObjectId;
+import use_case.comment.application_business_rules.CommentInputBoundary;
+import use_case.comment.application_business_rules.CommentInputData;
+
+import java.util.List;
+
+public class CommentController {
+    final CommentInputBoundary commentInteractor;
+
+    public CommentController(CommentInputBoundary commentInteractor){
+        this.commentInteractor = commentInteractor;
+    }
+
+    public void execute(ObjectId parentId, ObjectId authorId, String body, List<String> qualifications, List<String> collaborationRoles){
+        CommentInputData inputData = new CommentInputData(parentId, authorId, body, qualifications, collaborationRoles);
+        commentInteractor.execute(inputData);
+    }
+}
