6b3b9f9a18cb06f3fdb122bf4ff425c9e2c586ca
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-11-18 18:59:41 -0500

    Realized there was no need for input data here so I took it out

--- src/main/java/use_case/view_profile/application_business_rules/ViewProfileInputData.java
+++ src/main/java/use_case/view_profile/application_business_rules/ViewProfileInputData.java
@@ -2,35 +2,4 @@ package use_case.view_profile.application_business_rules;
 
 public class ViewProfileInputData {
 
-    final private String username;
-
-    final private String name;
-
-    final private String email;
-
-    final private double rating;
-
-    public ViewProfileInputData(String username, String name, String email, double rating) {
-        this.username = username;
-        this.name = name;
-        this.email = email;
-        this.rating = rating;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public double getRating() {
-        return rating;
-    }
-
 }
