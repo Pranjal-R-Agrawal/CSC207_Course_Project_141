8823c89fb2d2e52281fec2c86e20bc64e6a75272
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-11-18 18:23:30 -0500

    Implemented the ViewProfileOutputData

---
+++ src/main/java/use_case/view_profile/application_business_rules/ViewProfileOutputData.java
@@ -0,0 +1,34 @@
+package use_case.view_profile.application_business_rules;
+
+public class ViewProfileOutputData {
+    private final String username;
+
+    private final String name;
+
+    private final String email;
+
+    private final double rating;
+
+    public ViewProfileOutputData(String username, String name, String email, double rating) {
+        this.username = username;
+        this.name = name;
+        this.email = email;
+        this.rating = rating;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+}
