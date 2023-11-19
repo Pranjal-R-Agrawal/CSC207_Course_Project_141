dc1c0f4f5007b609ebc5197d8813d262d9559f70
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-11-18 19:13:50 -0500

    added the prepareFailView method to the Output Boundary interface

--- src/main/java/use_case/view_profile/application_business_rules/ViewProfileOutputBoundary.java
+++ src/main/java/use_case/view_profile/application_business_rules/ViewProfileOutputBoundary.java
@@ -2,4 +2,6 @@ package use_case.view_profile.application_business_rules;
 
 public interface ViewProfileOutputBoundary {
     void prepareSuccessView(ViewProfileOutputData user);
+
+    void prepareFailView(String error);
 }
