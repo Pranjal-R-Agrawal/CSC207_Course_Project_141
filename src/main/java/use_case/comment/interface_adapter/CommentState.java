f677fad37069dcfd77857b5d078b5c796278ad9d
Yathusan <yathusank630@gmail.com>
2023-11-19 12:37:20 -0500

    Added parentId and parentPostId to CommentState

--- src/main/java/use_case/comment/interface_adapter/CommentState.java
+++ src/main/java/use_case/comment/interface_adapter/CommentState.java
@@ -1,9 +1,13 @@
 package use_case.comment.interface_adapter;
 
 
+import org.bson.types.ObjectId;
+
 public class CommentState {
     private String body;
     private String qualifications;
+    private ObjectId parentId;
+    private ObjectId parentPostId;
     private String errorMessage;
 
     public String getBody(){return body;}
@@ -17,6 +21,21 @@ public class CommentState {
         this.qualifications = qualifications;
         return this;
     }
+
+    public ObjectId getParentId() {return parentId;}
+
+    public CommentState setParentId(ObjectId parentId) {
+        this.parentId = parentId;
+        return this;
+    }
+
+    public ObjectId getParentPostId() {return parentPostId;}
+
+    public CommentState setParentPostId(ObjectId parentPostId) {
+        this.parentPostId = parentPostId;
+        return this;
+    }
+
     public String getErrorMessage(){return errorMessage;}
     public CommentState setErrorMessage(String errorMessage){
         this.errorMessage = errorMessage;
