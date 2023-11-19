0cd0e64b82dd8527868fcccc9c7e5b4fb6221688
Sidharth Sawhney <sidharth.sawhney6@gmail.com>
2023-11-19 02:02:13 -0500

    Modified Main.java to create GenerateIdea related objects

--- src/test/java/data_access/MongoDBDataAccessObjectTest.java
+++ src/test/java/data_access/MongoDBDataAccessObjectTest.java
@@ -1,24 +1,6 @@
 package data_access;
 
 import entity.User;
-<<<<<<<<< Temporary merge branch 1
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-public class MongoDBDataAccessObjectTest extends MongoDBDataAccessObject {
-    public MongoDBDataAccessObjectTest() {
-            super(
-                    "src/main/java/data_access/database_connection.txt",
-                    "Tests", "Users", "Posts", "Comments"
-            );
-    }
-
-    @Test
-    public void testUsernameUsedEmptyCollection() {
-        assert !usernameUsed("username");
-=========
 import org.junit.Before;
 import org.junit.Test;
 
@@ -28,34 +10,23 @@ public class MongoDBDataAccessObjectTest {
     @Test
     public void testUsernameUsedEmptyCollection() {
         assert !dataAccessObject.usernameUsed("username");
->>>>>>>>> Temporary merge branch 2
     }
 
     @Test
     public void testAddUserOne() {
-        User user = new User("username", "password", "", "", "");
-<<<<<<<<< Temporary merge branch 1
-        addUser(user);
-        assert usernameUsed("username");
-    }
-
-    @Before
-    @After
-    public void resetDatabase() {
-        users.drop();
-        posts.drop();
-        comments.drop();
-    }
-}
-=========
+        User user = new User("username", "password", "", "", "", "", "");
         dataAccessObject.addUser(user);
         assert dataAccessObject.usernameUsed("username");
     }
 
     @Before
     public void setUpTest() {
-        dataAccessObject = new MongoDBDataAccessObjectBuilder().setTestParameters().build();
+        try {
+            dataAccessObject = new MongoDBDataAccessObjectBuilder().setTestParameters().build();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
         dataAccessObject.resetDatabase();
     }
 }
->>>>>>>>> Temporary merge branch 2
