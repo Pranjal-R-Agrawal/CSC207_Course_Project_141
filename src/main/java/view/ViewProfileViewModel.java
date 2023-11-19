562754b1c0a2a582ab9d4d61c72073dad75fc64c
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-11-18 19:31:27 -0500

    Extended from the ViewModel

--- src/main/java/view/ViewProfileViewModel.java
+++ src/main/java/view/ViewProfileViewModel.java
@@ -1,2 +1,8 @@
-package view;public class ViewProfileViewModel {
+package view;
+import use_case.view_profile.interface_adapter.ViewProfileState;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+public class ViewProfileViewModel extends ViewModel{
+
 }
