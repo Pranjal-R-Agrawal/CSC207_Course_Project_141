cd01976b1f43acf75452255ced760e9bb2ac3056
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-11-18 20:28:52 -0500

    Implemented the profile view and will add the property changes once declared in the presenter

---
+++ src/main/java/view/ViewProfileView.java
@@ -0,0 +1,38 @@
+package view;
+
+import use_case.view_profile.interface_adapter.ViewProfileController;
+import use_case.view_profile.interface_adapter.ViewProfileState;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class ViewProfileView extends JPanel implements PropertyChangeListener {
+    public final String viewName;
+    private final ViewProfileViewModel viewProfileViewModel;
+    private final ViewProfileController viewProfileController;
+    private final JTextField usernameDisplayField = new JTextField(20);
+    private final JTextField nameDisplayField = new JTextField(20);
+    private final JTextField emailDisplayField = new JTextField(20);
+    private final JTextField ratingsDisplayField = new JTextField(20);
+
+    private final JButton backButton = new JButton();
+    private final JButton logoutButton = new JButton();
+
+    public ViewProfileView(ViewProfileViewModel viewProfileViewModel, ViewProfileController viewProfileController) {
+        this.viewProfileViewModel = viewProfileViewModel;
+        this.viewProfileController = viewProfileController;
+
+        viewName = viewProfileViewModel.getViewName();
+        setName(viewName);
+        logoutButton.setText(ViewProfileViewModel.LOGOUT_BUTTON_LABEL);
+        backButton.setText(ViewProfileViewModel.BACK_BUTTON_LABEL);
+    }
+
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
+}
