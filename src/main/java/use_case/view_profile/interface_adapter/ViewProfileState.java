67081b03c2ad428663ee2b7ccea2634d4c113550
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-11-18 19:28:26 -0500

    Implemented the view profile state

---
+++ src/main/java/use_case/view_profile/interface_adapter/ViewProfileState.java
@@ -0,0 +1,45 @@
+package use_case.view_profile.interface_adapter;
+
+public class ViewProfileState {
+    private String username;
+    private String name;
+    private String email;
+    private double rating;
+
+    public String getUsername() {
+        return username;
+    }
+    public ViewProfileState setUsername() {
+        this.username = username;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ViewProfileState setName() {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public ViewProfileState setEmail() {
+        this.email = email;
+        return this;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public ViewProfileState setRating() {
+        this.rating = rating;
+        return this;
+    }
+
+
+}
