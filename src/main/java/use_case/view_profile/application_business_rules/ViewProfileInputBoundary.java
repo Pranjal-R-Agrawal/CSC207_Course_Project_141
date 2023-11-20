9e4876c71aff935337b2ef58f204d627eeb6bd44
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-11-20 16:41:20 -0500

    Took out the argument for the execute method in this file

--- src/main/java/use_case/view_profile/application_business_rules/ViewProfileInputBoundary.java
+++ src/main/java/use_case/view_profile/application_business_rules/ViewProfileInputBoundary.java
@@ -1,5 +1,5 @@
 package use_case.view_profile.application_business_rules;
 
 public interface ViewProfileInputBoundary {
-    void execute(ViewProfileInputData viewProfileInputData);
+    void execute();
 }
