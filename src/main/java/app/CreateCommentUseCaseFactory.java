f26f79bd494ccc076e226580f631c0e777c8c97f
Pranjal Agrawal <46265193+Pranjal-R-Agrawal@users.noreply.github.com>
2023-11-25 11:44:15 -0500

    Added another create function to CreateCommentUseCaseFactory for creating based on the parent's and paernt post's ids

--- src/main/java/app/CreateCommentUseCaseFactory.java
+++ src/main/java/app/CreateCommentUseCaseFactory.java
@@ -2,6 +2,7 @@ package app;
 
 import data_access.CreateCommentDataAccessInterface;
 import entity.CommentFactory;
+import org.bson.types.ObjectId;
 import use_case.create_comment.application_business_rules.CreateCommentInputBoundary;
 import use_case.create_comment.application_business_rules.CreateCommentInteractor;
 import use_case.create_comment.application_business_rules.CreateCommentOutputBoundary;
@@ -12,8 +13,21 @@ import view.CreateCommentViewModel;
 import view.DisplayCommentsViewModel;
 
 public class CreateCommentUseCaseFactory {
+    private DisplayCommentsViewModel displayCommentsViewModel;
+    private CreateCommentDataAccessInterface createCommentDataAccessObject;
 
     private CreateCommentUseCaseFactory() {}
+
+    public CreateCommentUseCaseFactory(DisplayCommentsViewModel displayCommentsViewModel, CreateCommentDataAccessInterface createCommentDataAccessObject) {
+        this.displayCommentsViewModel = displayCommentsViewModel;
+        this.createCommentDataAccessObject = createCommentDataAccessObject;
+    }
+
+    public CreateCommentView create(ObjectId parentPostId, ObjectId parentID) {
+        CreateCommentViewModel createCommentViewModel = new CreateCommentViewModel();
+        createCommentViewModel.getState().setParentPostId(parentPostId).setParentId(parentID);
+        return CreateCommentUseCaseFactory.create(displayCommentsViewModel, createCommentViewModel, createCommentDataAccessObject);
+    }
 
     public static CreateCommentView create(DisplayCommentsViewModel displayCommentsViewModel, CreateCommentViewModel createCommentViewModel,
                                            CreateCommentDataAccessInterface createCommentDataAccessObject){
