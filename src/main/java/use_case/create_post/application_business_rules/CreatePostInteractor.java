833cc9196c30c0f142885fc8490c054e1826de0b
Yathusan <yathusank630@gmail.com>
2023-11-27 05:50:29 -0500

    Created CreatePostInteractor

---
+++ src/main/java/use_case/create_post/application_business_rules/CreatePostInteractor.java
@@ -0,0 +1,39 @@
+package use_case.create_post.application_business_rules;
+
+import data_access.CreatePostDataAccessInterface;
+import entity.Post;
+import entity.PostFactory;
+import org.bson.types.ObjectId;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CreatePostInteractor implements CreatePostInputBoundary{
+    CreatePostOutputBoundary createPostPresenter;
+    CreatePostDataAccessInterface createPostDataAccessObject;
+    PostFactory postFactory;
+
+    public CreatePostInteractor(CreatePostOutputBoundary createPostPresenter, CreatePostDataAccessInterface createPostDataAccessObject, PostFactory postFactory){
+        this.createPostPresenter = createPostPresenter;
+        this.createPostDataAccessObject = createPostDataAccessObject;
+        this.postFactory = postFactory;
+    }
+
+    public void execute(CreatePostInputData createPostInputData){
+        if (createPostInputData.getTitle() == null || createPostInputData.getTitle().trim().isEmpty()){
+            createPostPresenter.prepareFailureView("Please enter a title.");
+        } else if (createPostInputData.getBody() == null || createPostInputData.getBody().trim().isEmpty()) {
+            createPostPresenter.prepareFailureView("Please enter text in body.");
+            // TODO: Validation for suggestedCollaboratorQualifications?
+        } else{
+            List<String> suggestedCollaboratorQualifications = Arrays.asList(createPostInputData.getSuggestedCollaboratorQualifications().split(";"));
+            suggestedCollaboratorQualifications.replaceAll(String::trim);
+            suggestedCollaboratorQualifications.removeIf(String::isEmpty);
+            ObjectId authorId = createPostDataAccessObject.getLoggedInUserId();
+            Post post = postFactory.create(authorId, createPostInputData.getTitle(), createPostInputData.getBody(), suggestedCollaboratorQualifications);
+            createPostDataAccessObject.addPost(post);
+            createPostPresenter.prepareSuccessView(post.getId());
+
+        }
+    }
+}
