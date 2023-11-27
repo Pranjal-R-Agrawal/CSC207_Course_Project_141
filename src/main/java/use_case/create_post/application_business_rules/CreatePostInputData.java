a2cdc2c3624e7bd121f93ba5234934182346f590
Yathusan <yathusank630@gmail.com>
2023-11-27 05:28:23 -0500

    Added CreatePostInputData

---
+++ src/main/java/use_case/create_post/application_business_rules/CreatePostInputData.java
@@ -0,0 +1,20 @@
+package use_case.create_post.application_business_rules;
+
+import org.bson.types.ObjectId;
+
+import java.util.List;
+
+public class CreatePostInputData {
+    final private String title;
+    final private String body;
+    final private String suggestedCollaboratorQualifications;
+    public CreatePostInputData(String title, String body, String suggestedCollaboratorQualifications){
+        this.title = title;
+        this.body = body;
+        this.suggestedCollaboratorQualifications = suggestedCollaboratorQualifications;
+    }
+
+    public String getBody(){return title;}
+    public String getTitle(){return body;}
+    public String getSuggestedCollaboratorQualifications() {return suggestedCollaboratorQualifications;}
+}
