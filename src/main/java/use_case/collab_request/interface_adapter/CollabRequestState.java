65bd0b1d206bb7a1c0a4d821c739903baf11ffad
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-11-27 03:27:38 -0500

    is the state  needed?

---
+++ src/main/java/use_case/collab_request/interface_adapter/CollabRequestState.java
@@ -0,0 +1,7 @@
+package use_case.collab_request.interface_adapter;
+
+import org.bson.types.ObjectId;
+
+public class CollabRequestState {
+    // TODO: Do I technically have a state? Since all I am doing is sending a request to the receiver.
+}
