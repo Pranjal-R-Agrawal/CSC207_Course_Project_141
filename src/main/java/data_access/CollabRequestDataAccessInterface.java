49d6c2450ba82581819865d6873ff33cf358462c
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-11-27 01:31:24 -0500

    added a setCollaborationRequestId method

--- src/main/java/data_access/CollabRequestDataAccessInterface.java
+++ src/main/java/data_access/CollabRequestDataAccessInterface.java
@@ -1,6 +1,11 @@
 package data_access;
 import entity.Comment;
+import entity.User;
+import entity.Post;
 import org.bson.types.ObjectId;
 public interface CollabRequestDataAccessInterface {
     Comment getCommentByCommentId(ObjectId id);
+    User getUserById(ObjectId id);
+    Post getPostByPostId(ObjectId id);
+    void setCollaborationRequestId(ObjectId id);
 }
