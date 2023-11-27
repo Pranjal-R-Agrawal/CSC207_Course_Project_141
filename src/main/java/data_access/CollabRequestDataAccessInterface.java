2743187d0b7cad747e3d2b2993c5e0c307492b66
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-11-27 00:43:34 -0500

    changed this so that it only needs the getCommentByCommentId

--- src/main/java/data_access/CollabRequestDataAccessInterface.java
+++ src/main/java/data_access/CollabRequestDataAccessInterface.java
@@ -1,11 +1,6 @@
 package data_access;
-import entity.User;
-import entity.Post;
+import entity.Comment;
 import org.bson.types.ObjectId;
 public interface CollabRequestDataAccessInterface {
-    User getUserByUsername(String username);
-    Post getPostByPostID(ObjectId id);
-
-
-
+    Comment getCommentByCommentId(ObjectId id);
 }
