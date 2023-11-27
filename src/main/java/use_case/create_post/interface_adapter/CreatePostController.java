71dcb1293a29cc22043e7970acf0da2fec1c9561
Yathusan <yathusank630@gmail.com>
2023-11-27 07:15:06 -0500

    Remove unused import statements

--- src/main/java/use_case/create_post/interface_adapter/CreatePostController.java
+++ src/main/java/use_case/create_post/interface_adapter/CreatePostController.java
@@ -1,6 +1,5 @@
 package use_case.create_post.interface_adapter;
 
-import use_case.create_comment.application_business_rules.CreateCommentInputBoundary;
 import use_case.create_post.application_business_rules.CreatePostInputBoundary;
 import use_case.create_post.application_business_rules.CreatePostInputData;
 import use_case.create_post.application_business_rules.CreatePostInteractor;
