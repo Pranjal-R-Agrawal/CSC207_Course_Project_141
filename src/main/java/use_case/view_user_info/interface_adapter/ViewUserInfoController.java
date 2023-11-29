c3b48a101c3e77e337c0aae0cf36bad3d2026c26
Tanmay-Shinde <tanmay_shinde@yahoo.com>
2023-11-29 00:35:54 -0500

    implemented controller for view user info use case

---
+++ src/main/java/use_case/view_user_info/interface_adapter/ViewUserInfoController.java
@@ -0,0 +1,20 @@
+package use_case.view_user_info.interface_adapter;
+
+import org.bson.types.ObjectId;
+import use_case.view_user_info.application_business_rules.ViewUserInfoInputBoundary;
+import use_case.view_user_info.application_business_rules.ViewUserInfoInputData;
+import use_case.view_user_info.application_business_rules.ViewUserInfoInteractor;
+
+public class ViewUserInfoController {
+
+    final ViewUserInfoInputBoundary viewUserInfoInteractor;
+
+    public ViewUserInfoController(ViewUserInfoInputBoundary viewUserInfoInteractor) {
+        this.viewUserInfoInteractor = viewUserInfoInteractor;
+    }
+
+    public void execute(ObjectId userId, boolean isCollaborator) {
+        ViewUserInfoInputData viewUserInfoInputData = new ViewUserInfoInputData(userId, isCollaborator);
+        ViewUserInfoInteractor.execute(viewUserInfoInputData);
+    }
+}
