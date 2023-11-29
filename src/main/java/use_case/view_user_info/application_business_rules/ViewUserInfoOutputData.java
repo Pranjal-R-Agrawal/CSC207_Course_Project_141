dd4d03626fd0f2d885ea57ace00cf8ba9e9e199b
Tanmay-Shinde <tanmay_shinde@yahoo.com>
2023-11-29 12:12:24 -0500

    implemented the Output Data class

--- src/main/java/use_case/view_user_info/application_business_rules/ViewUserInfoOutputData.java
+++ src/main/java/use_case/view_user_info/application_business_rules/ViewUserInfoOutputData.java
@@ -1,12 +1,35 @@
 package use_case.view_user_info.application_business_rules;
 
-import org.bson.types.ObjectId;
-
 public class ViewUserInfoOutputData {
 
-    private final ObjectId userId;
+    private final String username;
 
-    public ViewUserInfoOutputData(ObjectId userId) {
-        this.userId = userId;
+    private final double rating;
+
+    private final String email;
+
+    private final String phoneNumber;
+
+    public ViewUserInfoOutputData(String username, double rating, String email, String phoneNumber) {
+        this.username = username;
+        this.rating = rating;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 }
