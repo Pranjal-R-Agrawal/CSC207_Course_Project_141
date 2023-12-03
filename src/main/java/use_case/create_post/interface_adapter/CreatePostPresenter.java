88404b4e9508c3219ba032b8ede3736af484daea
Yathusan <yathusank630@gmail.com>
2023-12-03 17:16:04 -0500

    Made the presenter implement the correct interface

--- src/main/java/use_case/create_post/interface_adapter/CreatePostPresenter.java
+++ src/main/java/use_case/create_post/interface_adapter/CreatePostPresenter.java
@@ -1,15 +1,16 @@
 package use_case.create_post.interface_adapter;
 
 import org.bson.types.ObjectId;
-import use_case.create_comment.application_business_rules.CreateCommentOutputBoundary;
+import use_case.create_post.application_business_rules.CreatePostOutputBoundary;
 import view.CreatePostViewModel;
 import view.ViewManagerModel;
 
-public class CreatePostPresenter implements CreateCommentOutputBoundary {
+public class CreatePostPresenter implements CreatePostOutputBoundary {
     CreatePostViewModel createPostViewModel;
     ViewManagerModel viewManagerModel;
-    public CreatePostPresenter(CreatePostViewModel createPostViewModel){
+    public CreatePostPresenter(CreatePostViewModel createPostViewModel, ViewManagerModel viewManagerModel){
         this.createPostViewModel = createPostViewModel;
+        this.viewManagerModel = viewManagerModel;
     }
     @Override
     public void prepareSuccessView(ObjectId postId) {
