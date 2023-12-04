775ca12c2d0de70989f5e1362c06ee5173754902
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-04 16:41:03 -0500

    Took out the prepareFailView method since it was not being used

--- src/main/java/use_case/view_profile/application_business_rules/ViewProfileOutputBoundary.java
+++ src/main/java/use_case/view_profile/application_business_rules/ViewProfileOutputBoundary.java
@@ -3,5 +3,5 @@ package use_case.view_profile.application_business_rules;
 public interface ViewProfileOutputBoundary {
     void prepareSuccessView(ViewProfileOutputData user);
 
-    void prepareFailView(String error);
+
 }
