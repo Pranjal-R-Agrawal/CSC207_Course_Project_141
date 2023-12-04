db9f9d49235bf4d80530383a85b11fd5d3f7fceb
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-04 16:36:17 -0500

    Created the data file access object for the interactor integration test

---
+++ src/main/java/data_access/ViewProfileDataFileAccessObject.java
@@ -0,0 +1,58 @@
+package data_access;
+
+import entity.User;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ViewProfileDataFileAccessObject implements ViewProfileDataAccessInterface{
+    private final File userFile;
+    private final ArrayList<String> userContents;
+    private User testUser = null;
+
+    public ViewProfileDataFileAccessObject (String csvPath) throws Exception {
+        this.userFile = new File(csvPath);
+        this.userContents = new ArrayList<String>();
+        this.testUser = null;
+        try {
+            loadUser();
+        }
+        catch(FileNotFoundException e) {
+            throw new FileNotFoundException("Couldn't find database");
+        }
+
+    }
+    private void loadUser() throws Exception {
+        try (Scanner scanner = new Scanner(userFile)) {
+
+            while (scanner.hasNextLine()) {
+                String row = scanner.nextLine();
+                String[] items = row.split(",");
+                for (String item : items) {
+                    userContents.add(item.trim());
+                }
+            }
+
+            // Create the User object using the values from the CSV
+            String username = userContents.get(0);
+            String password = userContents.get(1);
+            String name = userContents.get(2);
+            String email = userContents.get(3);
+            String phoneNumber = userContents.get(4);
+            String city = userContents.get(5);
+            String fieldOfExpertise = userContents.get(6);
+            testUser = new User(username, password, name, email, phoneNumber, city, fieldOfExpertise);
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("Couldn't find database: " + userFile.getAbsolutePath());
+        }
+    }
+    @Override
+    public User getLoggedInUser() {
+        return testUser;
+
+    }
+}
