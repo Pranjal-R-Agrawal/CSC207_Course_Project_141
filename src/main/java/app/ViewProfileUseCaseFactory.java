3c9fcf4a63714689513dcbce7b7b204104293c08
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-04 18:01:19 -0500

    added view manager model argument to create method

--- src/main/java/app/ViewProfileUseCaseFactory.java
+++ src/main/java/app/ViewProfileUseCaseFactory.java
@@ -15,7 +15,7 @@ public class ViewProfileUseCaseFactory {
 
     public static ViewProfileView create(ViewManagerModel viewManagerModel, ViewProfileViewModel viewProfileViewModel, ViewProfileDataAccessInterface viewProfileDataAccessObject) {
         ViewProfileController viewProfileController = createViewProfileUseCase(viewManagerModel, viewProfileViewModel, viewProfileDataAccessObject);
-        return new ViewProfileView(viewProfileViewModel, viewProfileController);
+        return new ViewProfileView(viewProfileViewModel, viewProfileController, viewManagerModel);
     }
 
     private static ViewProfileController createViewProfileUseCase(ViewManagerModel viewManagerModel, ViewProfileViewModel viewProfileViewModel, ViewProfileDataAccessInterface viewProfileDataAccessObject) {
