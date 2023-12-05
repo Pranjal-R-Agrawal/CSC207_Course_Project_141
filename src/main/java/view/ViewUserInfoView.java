686ae1dade66f197b8b6bcb0d0c9c59260e24735
Tanmay-Shinde <tanmay_shinde@yahoo.com>
2023-12-05 17:29:00 -0500

    Added javadoc to ViewUserInfoView

--- src/main/java/view/ViewUserInfoView.java
+++ src/main/java/view/ViewUserInfoView.java
@@ -2,8 +2,16 @@ package view;
 
 import javax.swing.*;
 
+/**
+ * View class that displays a dialog box containing the relevant user info
+ * @author Tanmay Shinde
+ */
 public class ViewUserInfoView extends JPanel {
 
+    /**
+     * Displays a pop-up dialog box containing all relevant user information
+     * @param message the message to be displayed in the dialog box
+     */
     public void showUserInfo(String message) {
         JOptionPane.showMessageDialog(null, message, "User Info", JOptionPane.INFORMATION_MESSAGE);
     }
