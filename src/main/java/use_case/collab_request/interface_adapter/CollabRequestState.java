f82531d617469e88e4103b334a2cd20df236d20e
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-04 21:31:20 -0500

    realized this was not needed so I took it out

--- src/main/java/use_case/collab_request/interface_adapter/CollabRequestState.java
+++ src/main/java/use_case/collab_request/interface_adapter/CollabRequestState.java
@@ -3,5 +3,5 @@ package use_case.collab_request.interface_adapter;
 import org.bson.types.ObjectId;
 
 public class CollabRequestState {
-    // TODO: Do I technically have a state? Since all I am doing is sending a request to the receiver.
+
 }
