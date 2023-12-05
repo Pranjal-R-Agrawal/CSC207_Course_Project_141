9823ee2cf59dbf4cfe025f84a286c1a04e0a825c
Tanmay-Shinde <tanmay_shinde@yahoo.com>
2023-12-05 16:19:04 -0500

    added javadoc to ViewUserInfoDataAccessInterface

--- src/main/java/data_access/ViewUserInfoDataAccessInterface.java
+++ src/main/java/data_access/ViewUserInfoDataAccessInterface.java
@@ -3,8 +3,17 @@ package data_access;
 import entity.User;
 import org.bson.types.ObjectId;
 
+/*
+ * Represents a Data Access Object to access data from the database for the View User Info Use Case
+ * @author Tanmay Shinde
+ */
 public interface ViewUserInfoDataAccessInterface {
 
+    /*
+     * Retreives a User object from the database given a userId
+     * @param userId the id of the user that is to be retreived from the database
+     * @return a User object corresponding to the given userId
+     */
     User getUserById(ObjectId userId);
 
 }
