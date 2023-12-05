0693b57e0bbc785cea97648e9c2b47d09983a5f9
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-05 10:47:30 -0500

    added the posts and collab requests that should also be displayed in the view profile window

--- src/main/java/use_case/view_profile/application_business_rules/ViewProfileInteractor.java
+++ src/main/java/use_case/view_profile/application_business_rules/ViewProfileInteractor.java
@@ -2,8 +2,11 @@ package use_case.view_profile.application_business_rules;
 
 import data_access.ViewProfileDataAccessInterface;
 import java.util.ArrayList;
+import java.util.List;
+
 import entity.User;
 import entity.Post; // To get the needed Posts for collab requests?
+import org.bson.types.ObjectId;
 
 // TODO: Add the collab requests properly to the view profile use case
 public class ViewProfileInteractor implements ViewProfileInputBoundary {
@@ -24,8 +27,22 @@ public class ViewProfileInteractor implements ViewProfileInputBoundary {
         String name = viewProfileDataAccessObject.getLoggedInUser().getName();
         String email = viewProfileDataAccessObject.getLoggedInUser().getEmail();
         double rating = viewProfileDataAccessObject.getLoggedInUser().getRating();
+        String projects = "";
+        List<ObjectId> projectIds = viewProfileDataAccessObject.getLoggedInUser().getPostIDs();
+        for (ObjectId postId : projectIds) {
+            String post = viewProfileDataAccessObject.getPostByPostID(postId).getTitle();
+            projects += post + " ";
+        }
+        List<ObjectId> collabRequestIds = viewProfileDataAccessObject.getLoggedInUser().getCollaborationRequestIDs();
+        List<String> collabRequests = new ArrayList<>();
+        for (ObjectId collabRequestId : collabRequestIds) {
+            String collabRequest = viewProfileDataAccessObject.getCollabRequestById(collabRequestId).getTitle();
 
-        ViewProfileOutputData viewProfileOutputData = new ViewProfileOutputData(username, name, email, rating);
+            collabRequests.add(collabRequest);
+        }
+
+        ViewProfileOutputData viewProfileOutputData = new ViewProfileOutputData(username, name, email, rating, projects,
+                collabRequests);
         viewProfilePresenter.prepareSuccessView(viewProfileOutputData);
 
         }
