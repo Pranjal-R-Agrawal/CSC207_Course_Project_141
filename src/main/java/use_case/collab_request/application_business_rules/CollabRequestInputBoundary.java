bbe75987c76ca05749638a3b45c4b0491f96eea6
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-04 21:06:21 -0500

    added the arguments of postId and userId in the execute method for the input boundary

--- src/main/java/use_case/collab_request/application_business_rules/CollabRequestInputBoundary.java
+++ src/main/java/use_case/collab_request/application_business_rules/CollabRequestInputBoundary.java
@@ -1,5 +1,7 @@
 package use_case.collab_request.application_business_rules;
 
+import org.bson.types.ObjectId;
+
 public interface CollabRequestInputBoundary {
-    void execute();
+    void execute(ObjectId postId, ObjectId userId);
 }
