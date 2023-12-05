4422007063c2e324a9dd9125a66dad7b6f7fb664
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-04 23:24:32 -0500

    added the stuff that regards to the collab request use case

--- src/main/java/view/CollabRequestViewModel.java
+++
@@ -1,9 +0,0 @@
-package view;
-
-import use_case.collab_request.interface_adapter.CollabRequestState;
-
-public class CollabRequestViewModel {
-    // Do I have this? I'm wondering because from how I am imagining this, once the user clicks the collab request button,
-    // There would be a popup from the post and comment section saying something along the lines of "Collab Request sent!"
-    // or something like that. And that would be added onto the View of that respective use case.
-}
