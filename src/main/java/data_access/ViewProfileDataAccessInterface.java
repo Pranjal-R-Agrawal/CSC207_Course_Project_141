3fb3404e51b1febc3b9c478004779e4c7e889c64
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-05 10:45:38 -0500

    added some methods that were needed

--- src/main/java/data_access/ViewProfileDataAccessInterface.java
+++ src/main/java/data_access/ViewProfileDataAccessInterface.java
@@ -1,9 +1,13 @@
 package data_access;
+import entity.Post;
 import entity.User;
+import org.bson.types.ObjectId;
 
 
 public interface ViewProfileDataAccessInterface {
     User getLoggedInUser();
+    Post getPostByPostID(ObjectId id);
+    CollabRequest getCollabRequestById(ObjectId id);
 
 
 }
