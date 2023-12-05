857c43af5e1f99518aeff47b7c327ef4a5cd4180
Tanmay-Shinde <tanmay_shinde@yahoo.com>
2023-12-05 15:48:10 -0500

    Adapted the presenter to make use of the view class instead of directly displaying a JOptionPane dialog box

--- src/main/java/use_case/view_user_info/interface_adapter/ViewUserInfoPresenter.java
+++ src/main/java/use_case/view_user_info/interface_adapter/ViewUserInfoPresenter.java
@@ -2,6 +2,7 @@ package use_case.view_user_info.interface_adapter;
 
 import use_case.view_user_info.application_business_rules.ViewUserInfoOutputBoundary;
 import use_case.view_user_info.application_business_rules.ViewUserInfoOutputData;
+import view.ViewUserInfoView;
 
 import javax.swing.*;
 
@@ -10,23 +11,32 @@ public class ViewUserInfoPresenter implements ViewUserInfoOutputBoundary {
     @Override
     public void prepareCollabView(ViewUserInfoOutputData user) {
         String message = "<html>Name: </html>" + user.getName() +
+                "<html><br/>Field of Expertise: </html>" + user.getFieldOfExpertise() +
                 "<html><br/>Rating: </html>" + user.getRating() +
+                "<html><br/>City: </html>" + user.getCity() +
                 "<html><br/>Email: </html>" + user.getEmail() +
                 "<html><br/>Phone Number: </html>" + user.getPhoneNumber();
 
-        JOptionPane.showMessageDialog(null, message, "User Info", JOptionPane.INFORMATION_MESSAGE);
+        ViewUserInfoView viewUserInfoView = new ViewUserInfoView();
+
+        viewUserInfoView.showUserInfo(message);
     }
 
     @Override
     public void prepareGeneralView(ViewUserInfoOutputData user) {
         String message = "<html>Name: </html>" + user.getName() +
+                "<html><br/>Field of Expertise: </html>" + user.getFieldOfExpertise() +
                 "<html><br/>Rating: </html>" + user.getRating();
 
-        JOptionPane.showMessageDialog(null, message, "User Info", JOptionPane.INFORMATION_MESSAGE);
+        ViewUserInfoView viewUserInfoView = new ViewUserInfoView();
+
+        viewUserInfoView.showUserInfo(message);
     }
 
     @Override
     public void prepareFailView(String error) {
-        JOptionPane.showMessageDialog(null, error, "User Info", JOptionPane.INFORMATION_MESSAGE);
+        ViewUserInfoView viewUserInfoView = new ViewUserInfoView();
+
+        viewUserInfoView.showUserInfo(error);
     }
 }
