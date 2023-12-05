3500af24f1ea6ffdc62b65aebed2f9b9f4f8c601
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-04 20:43:31 -0500

    realized that there was no need for any input data of any sort so removed it

--- src/main/java/use_case/collab_request/application_business_rules/CollabRequestInputData.java
+++ src/main/java/use_case/collab_request/application_business_rules/CollabRequestInputData.java
@@ -1,16 +1,10 @@
 package use_case.collab_request.application_business_rules;
 
+import entity.User;
 import org.bson.types.ObjectId;
 
 public class CollabRequestInputData {
-    private final ObjectId commentId;
 
-    public CollabRequestInputData(ObjectId commentId) {
-        this.commentId = commentId;
-    }
 
-    public ObjectId getCommentId() {
-        return commentId;
-    }
 
 }
