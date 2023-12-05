291fa36ceb30e151266b933531c782fc2cd7315f
Yathusan <yathusank630@gmail.com>
2023-12-05 10:47:52 -0500

    Added interface of comment entity

---
+++ src/main/java/entity/CommentInterface.java
@@ -0,0 +1,87 @@
+package entity;
+
+import org.bson.types.ObjectId;
+
+import java.util.List;
+/**
+ * Represents comments
+ * @author Yathusan Koneswararajah
+ */
+public interface CommentInterface {
+    /**
+     * @return Returns ID of comment
+     */
+    public ObjectId getId();
+
+    /**
+     * Sets ID to comment
+     * @param id New ObjectId for comment
+     */
+    public void setId(ObjectId id);
+
+    /**
+     * @return Returns ID of top comment in the chain of current comment
+     */
+    public ObjectId getParentId();
+
+    /**
+     * Sets the parentID of this comment
+     * @param parentId
+     */
+    public void setParentId(ObjectId parentId);
+
+    /**
+     * @return Returns ID of post that comment is connected to
+     */
+    public ObjectId getParentPostId();
+
+    /**
+     * Sets ParentPostId of this comment
+     * @param parentPostId
+     */
+    public void setParentPostId(ObjectId parentPostId);
+
+    /**
+     * @return Returns list of IDs corresponding to comments under current comment
+     */
+    public List<ObjectId> getChildrenId();
+
+    /**
+     * Sets the list of IDs corresponding to comments under current comment
+     * @param childrenId
+     */
+    public void setChildrenId(List<ObjectId> childrenId);
+
+    /**
+     * @return Returns ID of author of comment
+     */
+    public ObjectId getAuthorId();
+
+    /**
+     * Sets ID of this comment's author
+     * @param authorId
+     */
+    public void setAuthorId(ObjectId authorId);
+
+    /**
+     * @return Returns the body of the comment
+     */
+    public String getBody();
+
+    /**
+     * Sets the body of the comment
+     * @param body
+     */
+    public void setBody(String body);
+
+    /**
+     * @return Returns qualifications in this comment
+     */
+    public List<String> getQualifications();
+
+    /**
+     * Sets the list of qualifications for this comment
+     * @param qualifications
+     */
+    public void setQualifications(List<String> qualifications);
+}
