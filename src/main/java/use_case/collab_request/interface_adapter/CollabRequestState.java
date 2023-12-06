a8800b85bed6245724b0ed2a08d14d7dea3dba74
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-06 04:27:12 -0500

    made CollabRequestState

--- src/main/java/use_case/collab_request/interface_adapter/CollabRequestState.java
+++ src/main/java/use_case/collab_request/interface_adapter/CollabRequestState.java
@@ -3,5 +3,25 @@ package use_case.collab_request.interface_adapter;
 import org.bson.types.ObjectId;
 
 public class CollabRequestState {
+    private ObjectId postId;
+    private ObjectId commenterId;
+
+    public CollabRequestState setPostId(ObjectId postId) {
+        this.postId = postId;
+        return this;
+    }
+    public ObjectId getPostId() {
+
+        return postId;
+    }
+    public CollabRequestState setCommenterId(ObjectId commenterId) {
+        this.commenterId = commenterId;
+        return this;
+    }
+    public ObjectId getCommenterId() {
+
+        return commenterId;
+    }
+
 
 }
