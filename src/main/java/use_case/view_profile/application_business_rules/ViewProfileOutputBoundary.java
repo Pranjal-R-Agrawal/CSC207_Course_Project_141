698571639520c43df085639f12e61d59b6de525a
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-06 16:00:50 -0500

    added javadoc

--- src/main/java/use_case/view_profile/application_business_rules/ViewProfileOutputBoundary.java
+++ src/main/java/use_case/view_profile/application_business_rules/ViewProfileOutputBoundary.java
@@ -1,6 +1,13 @@
 package use_case.view_profile.application_business_rules;
-
+/*
+ * Responsible for facilitating display of output data.
+ * @author Anbuselvan Ragunathan
+ */
 public interface ViewProfileOutputBoundary {
+    /**
+     * Directs components of the Interface Adapters to display the output data
+     * @param user - Output data (the ViewProfileOutputData object) to be displayed
+     */
     void prepareSuccessView(ViewProfileOutputData user);
 
 
