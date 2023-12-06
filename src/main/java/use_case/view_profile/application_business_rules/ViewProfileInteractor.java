61ec6edba213405c3a047e81079b256a93d052ea
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-06 15:59:44 -0500

    added javadoc

--- src/main/java/use_case/view_profile/application_business_rules/ViewProfileInteractor.java
+++ src/main/java/use_case/view_profile/application_business_rules/ViewProfileInteractor.java
@@ -8,21 +8,31 @@ import entity.User;
 import entity.Post; // To get the needed Posts for collab requests?
 import org.bson.types.ObjectId;
 
-// TODO: Add the collab requests properly to the view profile use case
+/**
+ * Concrete implementation of ViweProfileInputBoundary responsible for structuring the things that will be displayed in the view and passing it to the output boundary
+ * @author Anbuselvan Raguunathan
+ * */
 public class ViewProfileInteractor implements ViewProfileInputBoundary {
 
     final ViewProfileDataAccessInterface viewProfileDataAccessObject;
 
     final ViewProfileOutputBoundary viewProfilePresenter;
 
+    /**
+     * Initializes the ViewProfileInteractor object to perform the view profile logic
+     * @param viewProfileDataAccessObject Helps us interact appropriately with the Database
+     * @param viewProfilePresenter Receives the output data from ViewProfileInteractor to facilitate with the display of the output data
+     */
     public ViewProfileInteractor(ViewProfileDataAccessInterface viewProfileDataAccessObject, ViewProfileOutputBoundary viewProfilePresenter) {
         this.viewProfileDataAccessObject = viewProfileDataAccessObject;
         this.viewProfilePresenter = viewProfilePresenter;
     }
-
+    @Override
     public void execute() {
-        // TODO: Maybe change the rating to a string for displaying purposes?
 
+        /**
+         * Structures the things that will need to be displayed in the view
+         */
         String username = viewProfileDataAccessObject.getLoggedInUser().getUsername();
         String name = viewProfileDataAccessObject.getLoggedInUser().getName();
         String email = viewProfileDataAccessObject.getLoggedInUser().getEmail();
