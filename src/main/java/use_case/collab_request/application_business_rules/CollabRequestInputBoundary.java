d5a329a96e922e73394e82385ed461fb7cadee49
Sidharth Sawhney <sidharth.sawhney6@gmail.com>
2023-12-06 00:20:58 -0500

    Made modifications to the CollabRequestInteractor to facilitate testing. Completed the interactor test and integration test.

--- src/main/java/use_case/collab_request/application_business_rules/CollabRequestInputBoundary.java
+++ src/main/java/use_case/collab_request/application_business_rules/CollabRequestInputBoundary.java
@@ -1,7 +1,8 @@
 package use_case.collab_request.application_business_rules;
 
+import entity.User;
 import org.bson.types.ObjectId;
 
 public interface CollabRequestInputBoundary {
-    void execute(ObjectId postId, ObjectId userId);
+    void execute(ObjectId postId, ObjectId userId, User actualCommenterForTests);
 }
