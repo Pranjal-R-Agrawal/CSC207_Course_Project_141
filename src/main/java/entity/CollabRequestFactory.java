f47e9d9a33e106571ff75f1c1a2df23d04be0450
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-06 03:49:38 -0500

    Made code more collab request enttity focused abiding by factory design pattern.

--- src/main/java/entity/CollabRequestFactory.java
+++
@@ -1,7 +0,0 @@
-package entity;
-import org.bson.types.ObjectId;
-public class CollabRequestFactory {
-    public CollabRequest create(ObjectId postId, String author, String title) {
-        return new CollabRequest(postId, author, title);
-    }
-}
