ce22a515ddc1f4fb37cc4c8ce9bca6358e36360d
Tanmay-Shinde <tanmay_shinde@yahoo.com>
2023-12-05 19:44:48 -0500

    added javadoc to ViewUserInfoViewModel

--- src/main/java/view/ViewUserInfoViewModel.java
+++ src/main/java/view/ViewUserInfoViewModel.java
@@ -14,23 +14,42 @@ public class ViewUserInfoViewModel extends ViewModel{
     private ViewUserInfoState state = new ViewUserInfoState();
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
-    public ViewUserInfoViewModel(String viewName) {
+    /**
+     * Initializes the view model using the parent constructor
+     */
+    public ViewUserInfoViewModel() {
         super("view_user_info");
     }
 
+    /**
+     * Getter for state
+     * @return Returns a ViewUserInfoState object which has the current state (message) that needs to be displayed
+     */
     public ViewUserInfoState getState() {
         return state;
     }
 
+    /**
+     * Setter for state
+     * @param state the state value to be assigned to the ViewModel's state attribute
+     */
     public void setState(ViewUserInfoState state) {
         this.state = state;
     }
 
+    /**
+     * Instructs the Views listening to react based on the property change.
+     * @param propertyName the programmatic name of the property that was changed
+     */
     @Override
     public void firePropertyChanged(String propertyName) {
         support.firePropertyChange(propertyName, null, this.state);
     }
 
+    /**
+     * Configure an observer (View) of the ViewUserInfoViewModel
+     * @param listener the View willing to observe the changes in state in the ViewUserInfoViewModel
+     */
     @Override
     public void addPropertyChangeListener(PropertyChangeListener listener) {
         support.addPropertyChangeListener(listener);
