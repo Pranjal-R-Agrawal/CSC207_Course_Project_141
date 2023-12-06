90c5e6f79360fab13389ebeb1495e7dc34aeeb35
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-06 15:46:50 -0500

    added javadoc

--- src/main/java/data_access/ViewProfileDataAccessInterface.java
+++ src/main/java/data_access/ViewProfileDataAccessInterface.java
@@ -4,12 +4,34 @@ import entity.Post;
 import entity.User;
 import org.bson.types.ObjectId;
 
-
+/**
+ * Represents a Data Access Object to access data from the database for the View Profile Use Case
+ * @author Anbuselvan Ragunathan
+ */
 public interface ViewProfileDataAccessInterface {
+    /**
+     * Returns user from the database.
+     * @return the user that is currently logged in
+     */
     User getLoggedInUser();
+    /**
+     * Returns post from database
+     * @return the post that corresponds to the id
+     */
     Post getPostByPostID(ObjectId id);
+    /**
+     * Returns collab request from database
+     * @return the collab request that corresponds to the id
+     */
     CollabRequest getCollabRequestById(ObjectId id);
+    /**
+     * Add collab request to database
+     */
     void addCollabRequest(CollabRequest collabRequest);
+    /**
+     * Returns user from database
+     * @return the post that corresponds to the username
+     */
     User getUserByUsername(String username);
 
 }
