b769d37c30479b8a49184179b58dee52b98fe7e0
Tanmay-Shinde <tanmay_shinde@yahoo.com>
2023-12-05 22:24:32 -0500

    implemented the Use case factory and added javadoc

---
+++ src/main/java/app/SearchPostUseCaseFactory.java
@@ -0,0 +1,34 @@
+package app;
+
+import data_access.SearchPostsByTitleDataAccessInterface;
+import use_case.search_post.application_business_rules.SearchPostInputBoundary;
+import use_case.search_post.application_business_rules.SearchPostInteractor;
+import use_case.search_post.application_business_rules.SearchPostOutputBoundary;
+import use_case.search_post.interface_adapter.SearchPostController;
+import use_case.search_post.interface_adapter.SearchPostPresenter;
+import view.*;
+
+/**
+ * Static Factory class for assembling the required components for the Search Post use case
+ * @author Tanmay Shinde
+ */
+public class SearchPostUseCaseFactory {
+    /**
+     * Creates the view for the search post use case
+     * @param viewManagerModel Manages which view is displayed
+     * @param searchPostViewModel Observable that stores the state useful to the SearchPostView
+     * @param homePageViewModel Observable that stores the state useful to Home Page View
+     * @param searchPostDataAccessObject Data access object for interacting with the database
+     * @param createPostView the UI responsible for allowing the user to format a post
+     * @return a SearchPostView object configured using passed in arguments
+     */
+    public static SearchPostView create(ViewManagerModel viewManagerModel, SearchPostViewModel searchPostViewModel, HomePageViewModel homePageViewModel, SearchPostsByTitleDataAccessInterface searchPostDataAccessObject, CreatePostView createPostView) {
+        SearchPostController searchPostController = createSearchPostUseCase(viewManagerModel, searchPostViewModel, searchPostDataAccessObject);
+        return new SearchPostView(viewManagerModel, searchPostViewModel, homePageViewModel, searchPostController, createPostView);
+    }
+    private static SearchPostController createSearchPostUseCase(ViewManagerModel viewManagerModel, SearchPostViewModel searchPostViewModel, SearchPostsByTitleDataAccessInterface searchPostDataAccessObject) {
+        SearchPostOutputBoundary searchPostPresenter = new SearchPostPresenter(searchPostViewModel);
+        SearchPostInputBoundary searchPostInteractor = new SearchPostInteractor(searchPostDataAccessObject, searchPostPresenter);
+        return new SearchPostController(searchPostInteractor);
+    }
+}
