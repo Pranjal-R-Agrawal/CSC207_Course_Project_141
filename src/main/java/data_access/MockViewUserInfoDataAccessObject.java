518d1aadf48d4f7f0cd2ae580ca98626f277342a
Tanmay-Shinde <tanmay_shinde@yahoo.com>
2023-12-06 17:42:26 -0500

    implemented basic file structure for testing

--- src/main/java/data_access/MockViewUserInfoDataAccessObject.java
+++ src/main/java/data_access/MockViewUserInfoDataAccessObject.java
@@ -2,15 +2,15 @@ package data_access;
 
 import entity.User;
 import entity.UserFactory;
+import entity.UserInterface;
 import org.bson.types.ObjectId;
 
 public class MockViewUserInfoDataAccessObject implements ViewUserInfoDataAccessInterface{
 
     ObjectId userId = new ObjectId();
-    private UserFactory userFactory;
 
     public MockViewUserInfoDataAccessObject() {
-
+        UserInterface user = new UserFactory().create("testusername", "testpwd", "testname", "test@email.com", "0000000000", "testcity", "testexpertise");
     }
 
     @Override
