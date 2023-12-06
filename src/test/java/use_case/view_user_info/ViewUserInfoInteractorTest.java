1874b8c65dab80974bd55a89e1c1a0b12a3aa6a6
Tanmay-Shinde <tanmay_shinde@yahoo.com>
2023-12-06 18:29:13 -0500

    implemented interactor test for View User Info use case

--- src/test/java/use_case/view_user_info/ViewUserInfoInteractorTest.java
+++ src/test/java/use_case/view_user_info/ViewUserInfoInteractorTest.java
@@ -6,35 +6,111 @@ import org.bson.types.ObjectId;
 import use_case.view_user_info.application_business_rules.*;
 
 import org.junit.Test;
-import use_case.view_user_info.interface_adapter.ViewUserInfoPresenter;
 
 import static org.junit.Assert.*;
 
 
 public class ViewUserInfoInteractorTest {
 
+    @Test
     public void testIsCollaboratorCase() {
         ViewUserInfoInputData inputData = new ViewUserInfoInputData(new ObjectId(), true);
-        ViewUserInfoDataAccessInterface dataAccessObject = new MockViewUserInfoDataAccessObject();
+        ViewUserInfoDataAccessInterface mockDataAccessObject = new MockViewUserInfoDataAccessObject();
 
         ViewUserInfoOutputBoundary viewUserInfoPresenter = new ViewUserInfoOutputBoundary() {
             @Override
             public void prepareCollabView(ViewUserInfoOutputData user) {
-                assertEquals();
+                // "testname", "test@email.com", "0000000000", "testcity", "testexpertise"
+                assertEquals("testname", user.getName());
+                assertEquals("testexpertise", user.getFieldOfExpertise());
+                assertEquals("testcity", user.getCity());
+                assertEquals("test@email.com", user.getEmail());
+                assertEquals("0000000000", user.getPhoneNumber());
             }
 
             @Override
             public void prepareGeneralView(ViewUserInfoOutputData user) {
+
                 fail("General View is not expected.");
+
             }
 
             @Override
             public void prepareFailView(String error) {
+
                 fail("Use case fail is unexpected.");
+
             }
         };
 
-        ViewUserInfoInputBoundary interactor = new ViewUserInfoInteractor(dataAccessObject, viewUserInfoPresenter);
+        ViewUserInfoInputBoundary interactor = new ViewUserInfoInteractor(mockDataAccessObject, viewUserInfoPresenter);
+        interactor.execute(inputData);
+    }
+
+    @Test
+    public void testIsGeneralUserCase() {
+        ViewUserInfoInputData inputData = new ViewUserInfoInputData(new ObjectId(), false);
+        ViewUserInfoDataAccessInterface mockDataAccessObject = new MockViewUserInfoDataAccessObject();
+
+        ViewUserInfoOutputBoundary viewUserInfoPresenter = new ViewUserInfoOutputBoundary() {
+            @Override
+            public void prepareCollabView(ViewUserInfoOutputData user) {
+
+                fail("Collaborator View is not expected");
+
+            }
+
+            @Override
+            public void prepareGeneralView(ViewUserInfoOutputData user) {
+                // "testname", "test@email.com", "0000000000", "testcity", "testexpertise"
+                assertEquals("testname", user.getName());
+                assertEquals("testexpertise", user.getFieldOfExpertise());
+                assertEquals("", user.getCity());
+                assertEquals("", user.getEmail());
+                assertEquals("", user.getPhoneNumber());
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+
+                fail("Use case fail is unexpected.");
+
+            }
+        };
+
+        ViewUserInfoInputBoundary interactor = new ViewUserInfoInteractor(mockDataAccessObject, viewUserInfoPresenter);
+        interactor.execute(inputData);
+    }
+
+    @Test
+    public void testFailView() {
+        ViewUserInfoInputData inputData = new ViewUserInfoInputData(new ObjectId(), false);
+        ViewUserInfoDataAccessInterface mockDataAccessObject = new MockViewUserInfoDataAccessObject();
+
+        ViewUserInfoOutputBoundary viewUserInfoPresenter = new ViewUserInfoOutputBoundary() {
+            @Override
+            public void prepareCollabView(ViewUserInfoOutputData user) {
+
+                fail("Collaborator View is not expected");
+
+            }
+
+            @Override
+            public void prepareGeneralView(ViewUserInfoOutputData user) {
+
+                fail("General View is not expected.");
+
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+
+                assertEquals("Failed to retreive User", error);
+
+            }
+        };
+
+        ViewUserInfoInputBoundary interactor = new ViewUserInfoInteractor(mockDataAccessObject, viewUserInfoPresenter);
         interactor.execute(inputData);
     }
 }
