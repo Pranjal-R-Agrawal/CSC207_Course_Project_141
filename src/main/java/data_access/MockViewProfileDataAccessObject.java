9ceb255bba3ea21c59fb73c771125f844cf17c8b
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-06 15:49:47 -0500

    added javadoc

--- src/main/java/data_access/MockViewProfileDataAccessObject.java
+++ src/main/java/data_access/MockViewProfileDataAccessObject.java
@@ -7,9 +7,15 @@ import org.bson.types.ObjectId;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * Concrete implementation of the ViewProfileDataAccessInterface for testing purposes.
+ * @author Anbuselvan Ragunathan
+ */
 public class MockViewProfileDataAccessObject implements ViewProfileDataAccessInterface{
     @Override
+    /**
+     * Statically create a user
+     */
     public User getLoggedInUser() {
         User testUser = new User("testuser", "password123", "test", "test@email.com",
                 "123-456-7890", "testCity", "coding");
@@ -17,21 +23,34 @@ public class MockViewProfileDataAccessObject implements ViewProfileDataAccessInt
     }
 
     @Override
+    /**
+     * Returns a post from the database.
+     * @return an idea containing its prompt only (no business model)
+     */
     public Post getPostByPostID(ObjectId id) {
         return null;
     }
 
     @Override
+    /**
+     * not used
+     */
     public CollabRequest getCollabRequestById(ObjectId id) {
         return null;
     }
 
     @Override
+    /**
+     * not used
+     */
     public void addCollabRequest(CollabRequest collabRequest) {
 
     }
 
     @Override
+    /**
+     * not used
+     */
     public User getUserByUsername(String username) {
         return null;
     }
