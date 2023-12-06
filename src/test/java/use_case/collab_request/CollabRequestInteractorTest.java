9d8aaf1b8d3fae1956318c80cfc04ebad4ba30c6
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-05 23:41:16 -0500

    implemented interactor test

--- src/test/java/use_case/collab_request/CollabRequestInteractorTest.java
+++ src/test/java/use_case/collab_request/CollabRequestInteractorTest.java
@@ -1,43 +1,41 @@
-//package use_case.collab_request;
-//
-//import data_access.CollabRequestDataAccessInterface;
-//import data_access.MockCollabRequestDataAccessObject;
-//import entity.CollabRequestFactory;
-//import org.bson.types.ObjectId;
-//import org.junit.Before;
-//import org.junit.Test;
-//import use_case.collab_request.application_business_rules.CollabRequestInputBoundary;
-//import use_case.collab_request.application_business_rules.CollabRequestInteractor;
-//import use_case.collab_request.application_business_rules.CollabRequestOutputBoundary;
-//
-//import java.util.Objects;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertTrue;
-//
-//public class CollabRequestInteractorTest {
-//    public CollabRequestInteractorTest() {
-//
-//    }
-//
-//    @Test
-//    public void successTest() {
-//        CollabRequestDataAccessInterface collabRequestDataAccessObject = new MockCollabRequestDataAccessObject();
-//        CollabRequestFactory collabRequestFactory = new CollabRequestFactory();
-//        ObjectId objectId1 = new ObjectId();
-//        ObjectId objectId2 = new ObjectId();
-//        CollabRequestOutputBoundary successPresenter = new CollabRequestOutputBoundary() {
-//            @Override
-//            public void prepareSuccessView() {
-//                assertTrue(collabRequestDataAccessObject.getUserById(objectId2).getCollaborationRequestIDs().size() == 1);
-//            }
-//        };
-//
-//        CollabRequestInputBoundary interactor = new CollabRequestInteractor(collabRequestDataAccessObject, successPresenter, collabRequestFactory);
-//        interactor.execute(objectId1, objectId2);
-//
-//    }
-//
-//
-//
-//}
+package use_case.collab_request;
+
+import data_access.CollabRequestDataAccessInterface;
+import data_access.MockCollabRequestDataAccessObject;
+import entity.CollabRequestFactory;
+import org.bson.types.ObjectId;
+import org.junit.Before;
+import org.junit.Test;
+import use_case.collab_request.application_business_rules.CollabRequestInputBoundary;
+import use_case.collab_request.application_business_rules.CollabRequestInteractor;
+import use_case.collab_request.application_business_rules.CollabRequestOutputBoundary;
+
+import javax.swing.*;
+import java.util.Objects;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class CollabRequestInteractorTest {
+
+    @Test
+    public void successTest() {
+        MockCollabRequestDataAccessObject collabRequestDataAccessObject = new MockCollabRequestDataAccessObject();
+        CollabRequestFactory collabRequestFactory = new CollabRequestFactory();
+        ObjectId postid = collabRequestDataAccessObject.getIdPost();
+        ObjectId userid = collabRequestDataAccessObject.getIdUser();
+        CollabRequestOutputBoundary successPresenter = new CollabRequestOutputBoundary() {
+            @Override
+            public void prepareSuccessView() {
+                assertEquals(postid, collabRequestDataAccessObject.getUserById(userid).getCollaborationRequestIDs().get(0));
+            }
+        };
+
+        CollabRequestInputBoundary interactor = new CollabRequestInteractor(collabRequestDataAccessObject, successPresenter, collabRequestFactory);
+        interactor.execute(postid, userid);
+
+    }
+
+
+
+}
