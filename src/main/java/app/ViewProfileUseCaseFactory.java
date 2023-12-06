ad7d81b16a47002331776318353c4037e2554447
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-06 15:43:47 -0500

    added javadoc

--- src/main/java/app/ViewProfileUseCaseFactory.java
+++ src/main/java/app/ViewProfileUseCaseFactory.java
@@ -9,15 +9,25 @@ import view.ViewProfileViewModel;
 import view.ViewManagerModel;
 
 import javax.swing.*;
+/**
+ * Static Factory class for assembling the required components for the View Profile Use Case
+ * @author Anbuselvan Ragunathan
+ */
 public class ViewProfileUseCaseFactory {
-
+    /** Prevents instantiation */
     private ViewProfileUseCaseFactory() {}
 
     public static ViewProfileView create(ViewManagerModel viewManagerModel, ViewProfileViewModel viewProfileViewModel, ViewProfileDataAccessInterface viewProfileDataAccessObject) {
         ViewProfileController viewProfileController = createViewProfileUseCase(viewManagerModel, viewProfileViewModel, viewProfileDataAccessObject);
         return new ViewProfileView(viewProfileViewModel, viewProfileController, viewManagerModel,viewProfileDataAccessObject);
     }
-
+    /**
+     * Creates the View for the View Profile Use Case
+     * @param viewManagerModel Manages which view is displayed
+     * @param viewProfileViewModel Observable that stores the state useful to the View Profile View.
+     * @param viewProfileDataAccessObject Helps us interact appropriately with the Database
+     * @return the View for the View Profile Use Case
+     */
     private static ViewProfileController createViewProfileUseCase(ViewManagerModel viewManagerModel, ViewProfileViewModel viewProfileViewModel, ViewProfileDataAccessInterface viewProfileDataAccessObject) {
         ViewProfileOutputBoundary viewProfilePresenter = new ViewProfilePresenter(viewManagerModel, viewProfileViewModel);
         ViewProfileInputBoundary viewProfileInteractor = new ViewProfileInteractor(viewProfileDataAccessObject, viewProfilePresenter);
