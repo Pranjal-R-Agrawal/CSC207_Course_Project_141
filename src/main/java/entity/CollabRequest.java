292c335959256e2312a6129ffed4cbf3c8dcc870
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-06 05:09:09 -0500

    CollabRequest

--- src/main/java/entity/CollabRequest.java
+++ src/main/java/entity/CollabRequest.java
@@ -1,4 +1,15 @@
 package entity;
 
+import org.bson.types.ObjectId;
+
 public interface CollabRequest {
+
+    ObjectId getPostId();
+    void setPostId(ObjectId postId);
+    String getAuthor();
+    void setAuthor(String author);
+    String getTitle();
+    void setTitle(String title);
+    String getCommenter();
+    void setCommenter(String commenter);
 }
