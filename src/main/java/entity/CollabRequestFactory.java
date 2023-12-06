619a3f9687e311d551a7e785ebac1c0b42f5726c
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-06 05:09:45 -0500

    CollabRequestFactory

--- src/main/java/entity/CollabRequestFactory.java
+++ src/main/java/entity/CollabRequestFactory.java
@@ -1,4 +1,7 @@
 package entity;
 
+import org.bson.types.ObjectId;
+
 public interface CollabRequestFactory {
+    CollabRequest create(ObjectId postId, String commenter, String author, String title);
 }
