cf7e5391f9afacb1941a3c1c7429ffe8c0bd3adf
anburagu <anbuselvan.ragunathan@mail.utoronto.ca>
2023-12-06 22:50:24 -0500

    New files to support dialog box

---
+++ src/main/java/app/ViewProfileDialogUseCaseFactory.java
@@ -0,0 +1,26 @@
+package app;
+
+import data_access.ViewProfileDataAccessInterface;
+import use_case.view_profile.application_business_rules.ViewProfileInputBoundary;
+import use_case.view_profile.application_business_rules.ViewProfileInteractor;
+import use_case.view_profile.application_business_rules.ViewProfileOutputBoundary;
+import use_case.view_profile.interface_adapter.ViewProfileController;
+import use_case.view_profile.interface_adapter.ViewProfilePresenter;
+import view.*;
+
+
+public class ViewProfileDialogUseCaseFactory {
+    private ViewProfileDialogUseCaseFactory() {
+    }
+
+    public static ViewProfileDialogView createView(ViewProfileDialogViewModel viewProfileDialogViewModel) {
+        return new ViewProfileDialogView(viewProfileDialogViewModel);
+    }
+
+    public static ViewProfileController createController(ViewManagerModel viewManagerModel, ViewProfileDialogViewModel viewProfileDialogViewModel, ViewProfileDataAccessInterface viewProfileDataAccessObject) {
+        ViewProfileOutputBoundary viewProfilePresenter = new ViewProfilePresenter(viewManagerModel, viewProfileDialogViewModel);
+        ViewProfileInputBoundary viewProfileInteractor = new ViewProfileInteractor(viewProfileDataAccessObject, viewProfilePresenter);
+        return new ViewProfileController(viewProfileInteractor);
+
+    }
+}
---
+++ src/main/java/view/ViewProfileDialogView.java
@@ -0,0 +1,41 @@
+package view;
+
+
+import use_case.view_profile.interface_adapter.ViewProfileDialogState;
+
+import javax.swing.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+/**
+ * View class that displays a dialog box containing the relevant user info
+ * @author Tanmay Shinde
+ */
+public class ViewProfileDialogView extends JPanel implements PropertyChangeListener {
+
+    private final ViewProfileDialogViewModel viewProfileDialogViewModel;
+
+    public ViewProfileDialogView( ViewProfileDialogViewModel viewProfileDialogViewModel){
+        this.viewProfileDialogViewModel = viewProfileDialogViewModel;
+        viewProfileDialogViewModel.addPropertyChangeListener(this);
+    }
+    /**
+     * Displays a pop-up dialog box containing all relevant user information
+     * @param message the message to be displayed in the dialog box
+     */
+    public void showUserInfo(String message) {
+        JOptionPane.showMessageDialog(null, message, "User Profile", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    /**
+     * Calls the showUserInfo method passing the appropriate message from ViewUserInfoState when a change is made in the GenerateIdeaViewModel
+     * @param evt A PropertyChangeEvent object describing the event source and the property that has changed.
+     */
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if(evt.getPropertyName().equals("view_profile_display")) {
+            ViewProfileDialogState currentState = viewProfileDialogViewModel.getState();
+            showUserInfo(currentState.getMessage());
+        }
+    }
+}
