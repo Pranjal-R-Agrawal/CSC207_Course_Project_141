921cb096674b386bc356e252da8e15b179a0999e
Tanmay-Shinde <tanmay_shinde@yahoo.com>
2023-12-06 20:06:59 -0500

    improved functionality and added javadoc

--- src/main/java/data_access/MockViewUserInfoDataAccessObject.java
+++ src/main/java/data_access/MockViewUserInfoDataAccessObject.java
@@ -7,16 +7,39 @@ import org.bson.types.ObjectId;
 
 import java.util.HashMap;
 
+/**
+ * Mock DAO for view_user_info interactor test
+ * @author Tanmay Shinde
+ */
 public class MockViewUserInfoDataAccessObject implements ViewUserInfoDataAccessInterface{
 
     HashMap<Integer, User> users;
 
+    /**
+     * Initializes a new Hashmap which represents a mock database for testing
+     */
     public MockViewUserInfoDataAccessObject() {
+
         users = new HashMap<>();
-        User user = (User) new UserFactory().create("testusername", "testpwd", "testname", "test@email.com", "0000000000", "testcity", "testexpertise");
-        users.put(0, user);
+        populateHashMap();
+
     }
 
+    /**
+     * Populates the HashMap with a test user
+     */
+    public void populateHashMap() {
+
+        User user = (User) new UserFactory().create("testusername", "testpwd", "testname", "test@email.com", "0000000000", "testcity", "testexpertise");
+        users.put(0, user);
+
+    }
+
+    /**
+     * Concrete implementation of the getUserById function from the interface
+     * @param userId the id of the user that is to be retreived from the database
+     * @return the corresponding User object if it exists and null otherwise
+     */
     @Override
     public User getUserById(ObjectId userId) {
 
@@ -30,6 +53,9 @@ public class MockViewUserInfoDataAccessObject implements ViewUserInfoDataAccessI
 
     }
 
+    /**
+     * Clears the HashMap and resets it to represent a new empty HashMap
+     */
     public void drop(){
         users.clear();
         users = new HashMap<>();
